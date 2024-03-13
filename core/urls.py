e5e4ec2b1673e25cbeec6fc3550639e38c311469
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-13 18:39:49 +0200

    Setting up templates, routing, urls. Adding base.html and index.html in main app

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from apps.main import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  #gogol.com/admin/
+    path('', include('apps.main.urls'))  #gogol.com/
 ]
