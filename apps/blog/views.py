4e4ef58536cc751654b19eff1843a9ca7edea7ec
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-14 16:15:30 +0200

    Refactoring and imporing looking of all pages. Add navbar fro all ones

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,6 +6,9 @@ app_name = 'blog'
 def index(request):
 
     elements = {
-        "elements": ['Пайтон', 'Css', 'Js', 'C++', 'C#']
+        "elements": ({'id': 1, 'text':'Python'}, {'id': 2, 'text':'C#'}, {'id': 3, 'text':'C++'}, {'id': 4, 'text':'Java'})
     }
+    # elements = {
+    #     'elements': ({'id': 1, 'text':'Hello'}, {'id': 2, 'text':'Hello'}, {'id': 3, 'text':'Hello'}, {'id': 4, 'text':'Hello'})
+    # }
     return render(request, 'blog/index.html', elements)
