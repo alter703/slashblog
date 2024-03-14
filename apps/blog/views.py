ba7bfd30198e548a7e47c1859068d7344b85dc7a
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-14 20:22:01 +0200

    Update urls and views in blog

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 app_name = 'blog'
 
 # Create your views here.
-def index(request):
+def index(request, id):
 
     elements = {
         "elements": ({'id': 1, 'text':'Python'}, {'id': 2, 'text':'C#'}, {'id': 3, 'text':'C++'}, {'id': 4, 'text':'Java'})
     }
-    # elements = {
-    #     'elements': ({'id': 1, 'text':'Hello'}, {'id': 2, 'text':'Hello'}, {'id': 3, 'text':'Hello'}, {'id': 4, 'text':'Hello'})
-    # }
+
+    print(id)
     return render(request, 'blog/index.html', elements)
