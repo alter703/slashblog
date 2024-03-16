5e2ca89839d6ebe252e91bab6b85bea1968846d2
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-16 18:47:04 +0200

    Bug fix

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from django.contrib.auth import login, logout
-from django. contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
